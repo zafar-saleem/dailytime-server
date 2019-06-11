ee4f6611b320f1ea8e7213d51febc1cf0a9d9f8d
Zafar Saleem <zafarsaleem3@gmail.com>
2019-06-11 10:27:23 -0400

    removed admin from employees list

--- server/services/admin/list.js
+++ server/services/admin/list.js
@@ -3,7 +3,13 @@ const User = require('../../models/User');
 function fetch(request, response) {
   User.find({}, (error, docs) => {
     if (error) return response.json(error.message);
-    return response.json(docs);
+    let employees = [];
+    docs.map(employee => {
+      if (employee.role !== 'Admin') {
+        employees.push(employee);
+      }
+    });
+    return response.json(employees);
   });
 }
 

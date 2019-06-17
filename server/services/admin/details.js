99fe2fbfc49379a9dc7e2ea9e0a468243791d722
Zafar Saleem <zafarsaleem3@gmail.com>
2019-06-17 14:06:36 -0400

    Return employee's work hours alongside their profile

--- server/services/admin/details.js
+++ server/services/admin/details.js
@@ -1,9 +1,16 @@
 const User = require('../../models/User');
+const Hours = require('../../models/Hours');
 
 function fetch(request, response) {
+  let data = {};
   User.findOne({ _id: request.query.id }, (error, doc) => {
     if (error) return response.json(error.message);
-    return response.json(doc);
+    data['user'] = doc;
+    Hours.find({ userid: request.query.id }, (err, docs) => {
+      if (err) return response.json(err.message);
+      data['hours'] = docs;
+      return response.json(data);
+    })
   });
 }
 
